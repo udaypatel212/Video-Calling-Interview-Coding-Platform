18ef28f0d6ce6747b15c0e4873845321b72e8308
udaypatel212 <udayp20876@gmail.com>
2025-12-19 02:31:07 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,23 @@ const app = express();
 require('dotenv').config();  // to use environment variables from .env file
 // we have to perform this in every file where we want to use process.env
 
-app.get('/', (req, res) => {
-  res.status(200).send('workss');
-});
+const path = require('path');
+// const __dirname = path.resolve();
+
+// app.get('/', (req, res) => {
+//   res.status(200).send('workss');
+// });
 app.get('/health', (req, res) => {
   res.status(200).send('api is healthy ');
 });
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'../frontend/dist')));
+
+  app.get("/{*any}", (req,res) => {
+    res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
+  });
+} 
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server is running on port', process.env.PORT || 5000);
