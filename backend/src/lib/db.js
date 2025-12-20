796c801602c102ca6fdabfd19736e5bdbdff42ac
udaypatel212 <udayp20876@gmail.com>
2025-12-20 10:42:25 +0530

    .

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 
 const connectDB= async()=>{
     try{
+        if(!process.env.MONGODB_URI){
+            throw new Error("MONGODB_URI is not defined in environment variables");
+        }
      const conn=   await mongoose.connect(process.env.MONGODB_URI);
         console.log(" ✅ MongoDB connected successfully : ");
     }catch(err){
