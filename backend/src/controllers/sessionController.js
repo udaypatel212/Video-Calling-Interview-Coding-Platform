202bf199d38b56b60b9e00963bb2ac8429a2bf74
udaypatel212 <udayp20876@gmail.com>
2025-12-21 17:32:01 +0530

    api routes

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -93,12 +93,20 @@ module.exports.joinSession = async (req, res) => {
         const clerkId = req.user.clerkId; //clerk id of the participant user
 
         const session = await sessionModel.findById(id);
-        if (!session) {
+        if (!session ) {
             return res.status(404).json({ message: "Session not found" });
+        }
+        if( session.status !== "active") {
+            return res.status(400).json({ message: "Cannot join a completed session" });
         }
         if (session.participant) {
             return res.status(400).json({ message: "Session already has a participant" });
         }
+
+        if(session.host.toString() === userId.toString()) {
+            return res.status(400).json({ message: "Host cannot join as participant in their own session" });
+        }
+  
         session.participant = userId;
         await session.save();
 
@@ -146,16 +154,16 @@ module.exports.endSession = async (req, res) => {
             return res.status(400).json({ message: "Session is already completed" });
         }
 
-        session.status = "completed";
-        await session.save();
-       
+        
         //delete stream video call and chat channel
         const call= streamClient.video.call("default", session.callId);
         await call.delete({hard:true});
-           
+        
         const channel= chatClient.channel("messaging", session.callId);
         await channel.delete();
-
+        
+        session.status = "completed";
+        await session.save();
         return res.status(200).json({ message: "Session ended successfully", session });
 
     } catch (err) {
